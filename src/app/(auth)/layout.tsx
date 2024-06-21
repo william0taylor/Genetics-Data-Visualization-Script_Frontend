2e6bff3b0122ca3273c3ca77748a6f1091ebc3c0
william0taylor <william.taylor.win@gmail.com>
2024-06-21 16:58:16 -0700

    refactor: register form

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,8 @@
-export default function AuthLayout({children}:{children:React.ReactNode}) {
+export default function AuthLayout(props:{children:React.ReactNode}) {
     return (
         <div className="flex h-screen items-center justify-center">
             <div className="flex w-full max-w-lg flex-col justify-center rounded-xl bg-white px-4 py-10 shadow-lg">
-                {children}
+                {props.children}
             </div>
         </div>
     )
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link"
-export default function Page() {
+
+import RegisterForm from './_component/RegisterForm';
+
+export default function Register() {
     return (
         <>
             <div className="mx-auto">
@@ -8,62 +11,18 @@ export default function Page() {
             </h2>
             </div>
             <div className="mx-auto mt-10 w-full max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
-                <div>
-                    <p className="text-sm font-medium leading-6">Eamil Address</p>
-                    <div className="mt-2">
-                    <input
-                        className="w-full rounded-md border-0 p-2 text-sm outline-none ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-                        required
-                        type="text"
-                    />
-                    </div>
-                </div>
-                <div>
-                    <p className="text-sm font-medium leading-6">Password</p>
-                    <div className="mt-2">
-                    <input
-                        type="text"
-                        className="w-full rounded-md border-0 p-2 text-sm outline-none ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-                        required
-                    />
-                    </div>
-                </div>
-                <div className="flex items-center justify-between">
-                    <div className="flex gap-2">
-                    <input type="checkbox" />
-                    <p className="text-sm font-medium leading-6">Remeber me</p>
-                    </div>
-                    <div>
-                    <a
-                        href="/"
-                        className="text-sm font-medium leading-6 text-indigo-600 hover:text-indigo-500"
-                    >
-                        Forgot Password?
-                    </a>
-                    </div>
-                </div>
-                <div>
-                    <button
-                    type="submit"
-                    className="w-full rounded-md bg-indigo-600 py-2 text-sm font-semibold leading-4 text-white outline-none hover:bg-indigo-500"
-                    >
-                    Sign In
-                    </button>
-                </div>
-            </form>
+                <RegisterForm />
             </div>
-
             <div className="mx-auto mt-2 flex gap-2">
-            <p className="text-sm font-medium leading-6">Already have an account?</p>
-            <div>
-                <Link
-                href="/"
-                className="text-sm font-medium leading-6 text-indigo-600 hover:text-indigo-500"
-                >
-                    Sign in
-                </Link>
-            </div>
+                <p className="text-sm font-medium leading-6">Already have an account?</p>
+                <div>
+                    <Link
+                    href="/"
+                    className="text-sm font-medium leading-6 text-indigo-600 hover:text-indigo-500"
+                    >
+                        Sign in
+                    </Link>
+                </div>
             </div>
         </>
     )

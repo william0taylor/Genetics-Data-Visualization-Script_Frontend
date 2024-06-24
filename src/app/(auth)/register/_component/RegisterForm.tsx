f03d521b42b564e04fdd7f09d7e81307f92790ce
william0taylor <william.taylor.win@gmail.com>
2024-06-24 16:50:50 -0700

    refactor: validate regsiterForm

--- src/app/(auth)/register/_component/RegisterForm.tsx
+++ src/app/(auth)/register/_component/RegisterForm.tsx
@@ -1,23 +1,38 @@
 "use client"
 
 import React, { useState } from 'react'
-
+import { useRouter } from 'next/navigation';
+import registerUser from '@/app/api/auth/registerUser';
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 export default function RegisterForm() {
-
     const [formData, setFormData] = useState({
         name:"",
         email:"",
         password:"",
-        password2:"",
-    })
+        confirmPassword:"",
+    });
+
+    // validate register user 
+
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().required("Full Name is required"),
+        email: Yup.string().required("Email is required").email("Email is invalid"),
+        password: Yup.string().required("Password is required").length(6, "Passoword must be 6 characters at least"),
+        confirmPassword: Yup.string().required("Confirm Password is required").oneOf([Yup.ref("password"), ''], "Password must be matched"),
+      });
+      
+    const formOptions = { resolver: yupResolver(validationSchema) };
+    // get functions to build form with useForm() hook
+    const { register, handleSubmit, formState } = useForm(formOptions);
+
+    const { errors } = formState;
 
     const handleInput = (e:any) => {
-        const name = e.target.name;
-        const value = e.target.value;
-
         setFormData((prevState) => ({
             ...prevState,
-            [name]: value,
+            [e.target.name]: e.target.value,
         }));
     }
 
@@ -26,99 +41,87 @@ export default function RegisterForm() {
             name:"",
             email:"",
             password:"",
-            password2:"",
+            confirmPassword:"",
         })
-    }
-    const handleSubmit = (e:any) => {
-        e.preventDefault()
+    };
 
+    const onSubmit = () => {
         const newUser = {
             name:formData.name,
             email:formData.email,
             password:formData.password,
-            password2:formData.password2,
-        }      
+            confirmPassword:formData.confirmPassword,
+        };
 
-        console.log(newUser);
-        
+        registerUser(newUser);
 
         removeState();
-    }
+    };
 
     return (
-        <form className="space-y-6" onSubmit={handleSubmit}>
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <p className="text-sm font-medium leading-6">Full Name</p>
                 <div className="mt-2">
-                <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    className="w-full rounded-md border-0 p-2 text-sm outline-none ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-                    required
-                    onChange={handleInput}
-                />
+                    <input
+                        type="text"
+                        {...register("name")}
+                        name="name"
+                        value={formData.name}
+                        className="w-full rounded-md border-0 p-2 text-sm outline-none ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+                        onChange={handleInput}
+                    />
+                    <div className="text-red-500 mt-1 text-xs">{errors.name?.message}</div>
                 </div>
             </div>
             <div>
                 <p className="text-sm font-medium leading-6">Eamil Address</p>
                 <div className="mt-2">
-                <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    className="w-full rounded-md border-0 p-2 text-sm outline-none ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-                    required
-                    onChange={handleInput}
-                />
+                    <input
+                        type="text"
+                        {...register("email")}
+                        name="email"
+                        value={formData.email}
+                        className="w-full rounded-md border-0 p-2 text-sm outline-none ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+                        onChange={handleInput}
+                    />
+                    <div className="text-red-500 mt-1 text-xs">{errors.email?.message}</div>
                 </div>
             </div>
             <div>
                 <p className="text-sm font-medium leading-6">Password</p>
                 <div className="mt-2">
-                <input
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    className="w-full rounded-md border-0 p-2 text-sm outline-none ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-                    required
-                    onChange={handleInput}
-                />
+                    <input
+                        type="password"
+                        {...register("password")}
+                        name="password"
+                        value={formData.password}
+                        className="w-full rounded-md border-0 p-2 text-sm outline-none ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+                        onChange={handleInput}
+                    />
+                    <div className="text-red-500 mt-1 text-xs">{errors.password?.message}</div>
                 </div>
             </div>
             <div>
                 <p className="text-sm afont-medium leading-6">Confirm Password</p>
                 <div className="mt-2">
-                <input
-                    type="password"
-                    name="password2"
-                    value={formData.password2}
-                    className="w-full rounded-md border-0 p-2 text-sm outline-none ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-                    required
-                    onChange={handleInput}
-                />
-                </div>
-            </div>
-            <div className="flex items-center justify-between">
-                <div className="flex gap-2">
-                <input type="checkbox" />
-                <p className="text-sm font-medium leading-6">Remeber me</p>
-                </div>
-                <div>
-                <a
-                    href="/"
-                    className="text-sm font-medium leading-6 text-indigo-600 hover:text-indigo-500"
-                >
-                    Forgot Password?
-                </a>
+                    <input
+                        type="password"
+                        {...register("confirmPassword")}
+                        name="confirmPassword"
+                        value={formData.confirmPassword}
+                        className="w-full rounded-md border-0 p-2 text-sm outline-none ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+                        onChange={handleInput}
+                    />
+                    <div className="text-red-500 mt-1 text-xs">{errors.confirmPassword?.message}</div>
                 </div>
             </div>
             <div>
                 <button
-                type="submit"
-                className="w-full rounded-md bg-indigo-600 py-2 text-sm font-semibold leading-4 text-white outline-none hover:bg-indigo-500"
+                    type="submit"
+                    className="w-full rounded-md bg-indigo-600 py-2 text-sm font-semibold leading-4 text-white outline-none hover:bg-indigo-500"
                 >
-                Sign In
+                    Sign Up
                 </button>
             </div>
         </form>

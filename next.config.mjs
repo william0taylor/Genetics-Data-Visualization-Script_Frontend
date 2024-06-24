80870272ff16fc528d7cf74b58ffaf865bb6cfb4
william0taylor <william.taylor.win@gmail.com>
2024-06-24 10:38:40 -0700

    refactor: set env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        BACKEND_URL_PORT:"http://localhost:5000",
+    }
+};
 
 export default nextConfig;

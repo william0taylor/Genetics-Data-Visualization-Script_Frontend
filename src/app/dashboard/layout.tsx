c3a3949026d0fa0089e1cd2754b0a45fe6210c28
william0taylor <william.taylor.win@gmail.com>
2024-06-24 05:32:29 -0700

    refactor: upgrade dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 export default function DashboardLayout({children}:{children:React.ReactNode}) {
     return (
-        <div className="absolute bg-white w-full h-full left-0 top-0">
+        <div className="absolute bg-white w-full h-full left-0 top-0 flex flex-col">
             <header className="bg-indigo-700 shadow-xl">
                 <div className="max-w-7xl mx-auto flex text-white justify-between items-center py-5 px-3">
                     <div>
@@ -14,7 +14,7 @@ export default function DashboardLayout({children}:{children:React.ReactNode}) {
                     </div>
                 </div>
             </header>
-            <div className="max-w-xl mx-auto py-28 px-3">
+            <div className="max-w-xl mx-auto flex flex-1 items-center px-3">
                 {children}
             </div>
         </div>

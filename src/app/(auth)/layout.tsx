d30075110c6b74815a56cd3500011797dead9ab3
william0taylor <william.taylor.win@gmail.com>
2024-06-25 08:32:40 -0700

    refactor: app layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 export default function AuthLayout(props:{children:React.ReactNode}) {
     return (
-        <div className="flex h-screen items-center justify-center">
+        <div className="flex absolute w-full h-full items-center justify-center">
             <div className="flex w-full max-w-lg flex-col justify-center rounded-xl bg-white px-4 py-10 shadow-lg">
                 {props.children}
             </div>

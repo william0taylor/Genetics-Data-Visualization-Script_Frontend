e782b090edaf21218a4098d19e9f483c32bbccc7
william0taylor <william.taylor.win@gmail.com>
2024-06-25 16:30:27 -0700

    refactor:redirect

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,21 +1,17 @@
-import Link from "next/link"
-import { Suspense } from "react"
+"use client"
+
+import { Suspense, useEffect } from "react"
 import Loading from "../../components/Loading"
+import Header from "@/components/Header"
+import { redirect } from "next/navigation"
 export default function DashboardLayout({children}:{children:React.ReactNode}) {
+    useEffect(() => {
+        if(!localStorage.token) redirect('/')
+    },[])
+
     return (
         <div className="absolute bg-white w-full h-full left-0 top-0 flex flex-col">
-            <header className="bg-indigo-700 shadow-xl">
-                <div className="max-w-7xl mx-auto flex text-white justify-between items-center py-5 px-3">
-                    <div>
-                        <h1 className="text-2xl font-bold">GeneticNamePlate</h1>
-                    </div>
-                    <div>
-                        <Link href="/" className="font-medium">
-                            Log Out
-                        </Link>
-                    </div>
-                </div>
-            </header>
+            <Header />
             <div className="max-w-xl mx-auto flex flex-1 items-center px-3">
                 <Suspense fallback={<Loading />}>
                     {children}

5239eb827c854a52080c9c7ab7cffd4e72f5d6ee
william0taylor <william.taylor.win@gmail.com>
2024-06-26 11:13:49 -0700

    refactor: Header and uploadCSV components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,10 +29,10 @@ export default function Header() {
         <header className="bg-indigo-700 shadow-xl">
             <div className="max-w-7xl mx-auto flex text-white justify-between items-center py-5 px-3 gap-2">
                 <div>
-                    <h1 className="text-lg xs:text-2xl font-bold">GeneticNamePlate</h1>
+                    <h1 className="text-lg sm:text-2xl font-bold">GeneticNamePlate</h1>
                 </div>
 
-                <div className="hidden xs:block">
+                <div className="hidden sm:block">
                     {link ? (
                         <Link href="/" className="font-medium" onClick={logOut}>
                             Log Out
@@ -42,7 +42,7 @@ export default function Header() {
                     }
                 </div>
 
-                <div className="xs:hidden block" onClick={logOut}>
+                <div className="sm:hidden block" onClick={logOut}>
                     <HiOutlineLogout size={20} />
                 </div>
             </div>

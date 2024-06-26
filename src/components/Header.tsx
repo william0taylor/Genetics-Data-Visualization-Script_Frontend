fa12129c47a88be1ed8d2214d6ef72de7ab386d7
william0taylor <william.taylor.win@gmail.com>
2024-06-26 09:08:03 -0700

    refactor: add logout button and icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 "use client"
 
-import { getTokenValid } from "@/utils/getTokenValid"
-import Link from "next/link"
-import { useEffect, useState } from "react"
-export default function Header() {
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { HiOutlineLogout } from "react-icons/hi";
+import { getTokenValid } from "@/utils/getTokenValid";
 
+export default function Header() {
+    const router = useRouter();
+    
     const [link, setLink] = useState(false);
 
     useEffect(() => {
@@ -14,25 +18,34 @@ export default function Header() {
 
         if(tokenValid) setLink(true);
         else setLink(false);
-    }, []);
+    }, [link]);
 
     const logOut = () => {
+        router.push('/')
         localStorage.removeItem('token');
-    }
+    };
+
     return (
         <header className="bg-indigo-700 shadow-xl">
-            <div className="max-w-7xl mx-auto flex text-white justify-between items-center py-5 px-3">
+            <div className="max-w-7xl mx-auto flex text-white justify-between items-center py-5 px-3 gap-2">
                 <div>
-                    <h1 className="text-2xl font-bold">GeneticNamePlate</h1>
+                    <h1 className="text-lg xs:text-2xl font-bold">GeneticNamePlate</h1>
                 </div>
-                <div>
+
+                <div className="hidden xs:block">
                     {link ? (
                         <Link href="/" className="font-medium" onClick={logOut}>
                             Log Out
                         </Link>
-                    ): null}
+                        ) : 
+                        null
+                    }
+                </div>
+
+                <div className="xs:hidden block" onClick={logOut}>
+                    <HiOutlineLogout size={20} />
                 </div>
             </div>
         </header>
     )
-}
+};

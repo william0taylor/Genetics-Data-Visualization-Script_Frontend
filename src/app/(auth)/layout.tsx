1219566fa7c0d673c87db456d9a250e524c88e13
william0taylor <william.taylor.win@gmail.com>
2024-06-26 09:05:56 -0700

    refactor: formatting layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,7 @@
 export default function AuthLayout(props:{children:React.ReactNode}) {
     return (
         <div className="flex absolute w-full h-full items-center justify-center">
-            <div className="flex w-full max-w-lg flex-col justify-center rounded-xl bg-white px-4 py-10 shadow-lg">
-                {props.children}
-            </div>
+            {props.children}
         </div>
     )
 }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,22 +1,26 @@
 "use client"
 
-import { Suspense, useEffect } from "react"
-import Loading from "../../components/Loading"
-import Header from "@/components/Header"
-import { redirect } from "next/navigation"
+import { Suspense, useEffect } from "react";
+import { redirect } from "next/navigation";
+import Loading from "@/components/Loading";
+import Header from "@/components/Header";
+
 export default function DashboardLayout({children}:{children:React.ReactNode}) {
     useEffect(() => {
         if(!localStorage.token) redirect('/')
-    },[])
+    },[]);
 
     return (
-        <div className="absolute bg-white w-full h-full left-0 top-0 flex flex-col">
+        <div className="absolute bg-white min-w-full min-h-full left-0 top-0 flex flex-col">
             <Header />
-            <div className="max-w-xl mx-auto flex flex-1 items-center px-3">
+            
+            <div className="w-full max-w-lg mx-auto flex flex-1 justify-center items-center p-3">
                 <Suspense fallback={<Loading />}>
-                    {children}
+                    <div className="w-full">
+                        {children}
+                    </div>
                 </Suspense>
             </div>
         </div>
     )
-}
+};
